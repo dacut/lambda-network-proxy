0e3d1767f3344bf2ddecb0d3dd6fe8ef669bbb11
David Cuthbert <dacut@kanga.org>
2022-02-05 12:32:37 -0800

    Add event files.

---
+++ event/event.go
@@ -0,0 +1,10 @@
+package event
+
+type ProxyEndpointEvent struct {
+	ClientProtocol string `json:"ClientProtocol"`
+	ClientAddress  string `json:"ClientAddress"`
+	ClientPort     uint   `json:"ClientPort"`
+	ProxyProtocol  string `json:"ProxyProtocol"`
+	ProxyAddress   string `json:"ProxyAddress"`
+	ProxyPort      uint   `json:"ProxyPort"`
+}
---
+++ event/go.mod
@@ -0,0 +1,3 @@
+module github.com/dacut/lambda-nlb/event
+
+go 1.17
